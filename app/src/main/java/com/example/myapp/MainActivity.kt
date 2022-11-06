9b2d42a823583ea667076a295d2048f6297f6612
Murugan K <murugank0204@gmail.com>
2022-11-06 11:54:06 +0530

    bix fixes

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -2,59 +2,68 @@ package com.example.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.constraintlayout.motion.widget.TransitionBuilder.validate
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_plus.setOnClickListener{
+        val firstNumber = findViewById<EditText>(R.id.edt_one)
+        val secondNumber = findViewById<EditText>(R.id.edt_two)
 
-        validate(edt_one.text.toString(), edt_two.text.toString(), operator: '+')
+        val btnPlus = findViewById<Button>(R.id.btn_plus)
+        val btnMinus = findViewById<Button>(R.id.btn_minus)
+        val btnMul = findViewById<Button>(R.id.btn_multiple)
+        val btnDiv = findViewById<Button>(R.id.btn_div)
 
+        btnPlus.setOnClickListener {
+             validate(firstNumber.text.toString(), secondNumber.text.toString(), '+')
         }
-        btn_mius.setOnClickListener{
-            validate(edt_one.text.toString(), edt_two.text.toString(), operator:'-')
-
+        btnMinus.setOnClickListener {
+            validate(firstNumber.text.toString(), secondNumber.text.toString(), '-')
         }
-        btn_multiple.setOnClickListener{
-            validate(edt_one.text.toString(), edt_two.text.toString(), operator: '*')
+        btnMul.setOnClickListener {
+            validate(firstNumber.text.toString(), secondNumber.text.toString(), '*')
+        }
+        btnDiv.setOnClickListener {
+            validate(firstNumber.text.toString(), secondNumber.text.toString(), '/')
+        }
+    }
+    private fun validate(first:String, second:String, operator:Char){
+        val firstNumber = findViewById<EditText>(R.id.edt_one)
+        val secondNumber = findViewById<EditText>(R.id.edt_two)
+        val resultSection = findViewById<TextView>(R.id.tv_result)
 
+        if (first.isEmpty()){
+            resultSection.text = "Add a Number!"
+        }
+        if (second.isEmpty()){
+            resultSection.text = "Add second number"
+        }
+        if (first.isNotEmpty() && second.isNotEmpty()){
+            calculation(first.toDouble(), second.toDouble(), operator)
+        }
+    }
 
-
+    private fun calculation(awal:Double, kedua:Double, operator:Char){
+        var result =0.0
+        when(operator){
+            '+' -> result = awal + kedua
+            '-' -> result = awal - kedua
+            '*' -> result = awal * kedua
+            '/' -> result = awal / kedua
         }
 
-        btn_div.setOnClickListener{
-            validate(edt_one.text.toString(), edt_two.text.toString(), operator: '/')
-
-        }
-
-        private fun validate(awal:String, kedua:String, operator:Char){
-            if (awal.isEmpty()){
-                edt_one.error="Add a Number!"
-
-            }
-            if (kedua.isEmpty()){
-                edt_two.error ="Add a Number!"
-            }
-            if (awal.isEmpty() && kedua.isNotEmpty()){
-                calculation(awal.toDouble(), kedua.toDouble(), operator)
-            }
-
-        }
-
-        private fun calculation(awal:Double, kedua:Double, operator:Char){
-            var result =0.0
-            when(operator){
-                '+' -> result = awal + kedua
-                '-' -> result = awal - kedua
-                '*' -> result = awal * kedua
-                '/' -> result = awal / kedua
-            }
-            val tv_resut = null
-            tv_resut.text = result.toString()
-        }
+        val resultSection = findViewById<TextView>(R.id.tv_result)
+        resultSection.text = result.toString()
     }
 }
 
